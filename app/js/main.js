a33477a6812e32c35f06d657c35cea474c3b346a
Saylar2210 <bochorov@mail.ru>
2020-09-25 21:15:23 +0300

    added footer section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,29 @@
 $(function () {
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly:true
+    });
+    $('.pruduct-slider__inner').slick({
+        dots:true,
+        arrows:false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
+
+    $(".js-range-slider").ionRangeSlider({
+       
+        type: "double",
+        
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+
+        
+
+      
+    });
     var mixer = mixitup('.products__inner-box');
 });
